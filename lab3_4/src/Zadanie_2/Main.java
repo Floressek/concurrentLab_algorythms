af09f0d699a25460b391956df9727de42af8d33d
szyme <szymek.ktosiek@gmail.com>
2024-03-25 15:36:39 +0100

    Corrected Mains

--- lab3_4/src/Zadanie_2/Main.java
+++ lab3_4/src/Zadanie_2/Main.java
@@ -7,10 +7,10 @@ class Main {
     public static void main(String[] args){
         int iterationCount = 10;
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Input 1 for synchronized mode, 0 for not synchronized mode: ");
-//        int mode = scanner.nextInt();
-        int mode = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input 1 for synchronized mode, 0 for not synchronized mode: ");
+        int mode = scanner.nextInt();
+//        int mode = 1;
 
         // Checks logic sentence
         boolean synchronizedMode = mode == 1;
--- lab3_4/src/Zadanie_4/Main.java
+++ lab3_4/src/Zadanie_4/Main.java
@@ -1,13 +1,15 @@
 package Zadanie_4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
         int iterationCount = 10;
 
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.println("Input 1 for synchronized mode, 0 for not synchronized mode: ");
-//        int mode = scanner.nextInt();
-        int mode = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input 1 for synchronized mode, 0 for not synchronized mode: ");
+        int mode = scanner.nextInt();
+//        int mode = 1;
 
         // Checks logic sentence
         boolean synchronizedMode = mode == 1;
