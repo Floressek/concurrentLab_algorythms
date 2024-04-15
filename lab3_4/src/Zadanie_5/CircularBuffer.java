d84d15d36af389eb8641a19f2aa9cb55291f35f8
Szymon Florek <117664162+Floressek@users.noreply.github.com>
2024-04-15 18:58:07 +0000

    new exercise for the producer - consumer problem

---
+++ lab3_4/src/Zadanie_5/CircularBuffer.java
@@ -0,0 +1,57 @@
+package Exercise_1;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class CircularBuffer {
+    // Shared space for consumer and producer
+    private final String[] buffer;
+    // Cyclic buffering, head is for consumer (deleting), tail is for producer (adding)
+    private int head = 0;
+    private int tail = 0;
+    private final Semaphore semFree;
+    private final Semaphore semFull;
+    private final AtomicInteger producersFinished = new AtomicInteger(0);
+    private int numberOfConsumers;
+    private int totalProducers;
+
+    public CircularBuffer(int size, int totalProducers, int numberOfConsumers){
+        buffer = new String[size];
+        semFree = new Semaphore(size);
+        // Initially, the buffer is empty
+        semFull = new Semaphore(0);
+        this.totalProducers = totalProducers;
+        this.numberOfConsumers = numberOfConsumers;
+    }
+
+    public void producersFinished() throws InterruptedException {
+        if (producersFinished.incrementAndGet() == totalProducers) {
+            for (int i = 0; i < numberOfConsumers; i++) {
+                produce("POISON_PILL");
+            }
+        }
+    }
+    public void produce(String item) throws InterruptedException {
+        // Wait until there is a free space in the buffer
+        semFree.acquire();
+        synchronized (this) {
+            buffer[tail] = item;
+            // Move the tail to the next position
+            tail = (tail + 1) % buffer.length;
+        }
+        semFull.release();
+    }
+
+    public String consume() throws InterruptedException{
+        // Wait until there is an item in the buffer
+        semFull.acquire();
+        String item;
+        synchronized (this) {
+            item = buffer[head];
+            // Still moves the node to the next position(same as tail) difference in semantics
+            head = (head + 1) % buffer.length;
+        }
+        semFree.release();
+        return item;
+    }
+}
